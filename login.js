1993928718bc21a2ea1adb0956c007160de7920d
Vanshika <vanshikaagarwal544@gmail.com>
2021-01-21 16:38:29 +0530

    Updated

--- login.js
+++ login.js
@@ -3,8 +3,11 @@ function loginNow(){
 	var userEmail = document.getElementById('userLoginEmail').value;
 	var userPassword = document.getElementById('userLoginPassword').value;
 	var login = document.getElementById('btnlogin');
-	if(login){
+	
 		var adminDetails = JSON.parse(localStorage.getItem('adminRegistrationDetails'));
+		var userDetails = JSON.parse(localStorage.getItem('userLoginDetails'));
+	var temp;
+	if(login){
 	if(adminDetails !== null){
 			
 
@@ -14,7 +17,10 @@ function loginNow(){
 				if(userPassword === adminDetails.password){
 				window.open('admin-dashboard.html'); //because of this session is not working
 				//window.location.href = "/admin-dashboard.html";
-				sessionStorage.setItem("name", temp.name);	
+				var loginTime = { username : temp,
+					time : new Date().toLocaleTimeString()};
+				sessionStorage.setItem("name", temp.name);
+				sessionStorage.setItem('loginTime',JSON.stringify(loginTime));	
 			}
 				else{
 					alert('Enter the correct password');
@@ -23,8 +29,24 @@ function loginNow(){
 			
 		}
 		else{
-				alert('Please register First');
-				window.open('register.html');}
+			for(i in userDetails){
+				temp = i.name;
+				if(i.email === userEmail){
+					if(userPassword === i.password){
+				window.open('subUser-dashboard.html'); //because of this session is not working
+				var loginTime = { username : temp,
+					time : new Date().toLocaleTimeString()};
+				sessionStorage.setItem("name", temp.name);
+				sessionStorage.setItem('loginTime',JSON.stringify(loginTime));	
+			}
+			else{
+					alert('Enter the correct password');
+				}
+
+				}
+			}
+		}
+		
 	}
 	
 }
