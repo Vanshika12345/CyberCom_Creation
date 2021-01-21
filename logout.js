ead9724568f11100e26584d8eae9c80239a9b3e6
Vanshika <vanshikaagarwal544@gmail.com>
2021-01-21 17:04:28 +0530

    Final Commit

--- logout.js
+++
@@ -1,6 +0,0 @@
-
-	var session = JSON.parse(sessionStorage.getItem('loginTime'));
-	loginTime.logoutTime = new Date().to LocaleTimeString();
-	window.open('Login.html');
-	
-	//var sessionData = JSON.parse(sessionStorage.getItem('loginTime');
