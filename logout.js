b6eb5827edee4d58b7e4bc00609447db537da3ee
Vanshika <vanshikaagarwal544@gmail.com>
2021-01-21 16:46:40 +0530

    Updates

--- logout.js
+++ logout.js
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<script type="text/javascript">
+
 	var session = JSON.parse(sessionStorage.getItem('loginTime'));
 	loginTime.logoutTime = new Date().to LocaleTimeString();
 	window.open('Login.html');
 	
-	//var sessionData = JSON.parse(sessionStorage.getItem('loginTime'));
-	
-</script>
-</body>
-</html>
+	//var sessionData = JSON.parse(sessionStorage.getItem('loginTime');
