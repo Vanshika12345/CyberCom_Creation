60a6a7531c4688a2e5ef4dd44aff750a3574b7e5
Vanshika <vanshikaagarwal544@gmail.com>
2021-01-22 19:55:58 +0530

    Practice of 22Jan Done

---
+++ practice/2021-01-22/forLoop.php
@@ -0,0 +1,11 @@
+<?php
+
+for($i = 0;$i < 3;i++){
+	echo "Inside for loop i is $i<br>";
+}
+
+$x = 1;
+for(;$x<3;x++){
+	echo "Inside for loop i is $i<br>";
+}
+?>
---
+++ practice/2021-01-22/switchStatement.php
@@ -0,0 +1,17 @@
+<?php
+
+$number = 1;
+switch($number){
+	case 1:
+		echo "One";
+			break;
+	case 2:
+		echo "Two";
+			break;
+	default:
+		echo "Number not present";
+			break;
+}
+
+
+?>
